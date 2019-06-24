e1b16c06135edb350cf49fb51b3061efa7d0c003
yanglong <598251812@qq.com>
2019-06-24 19:13:14 +0800

    commit

--- test_py/Helper/mongo_helper.py
+++ test_py/Helper/mongo_helper.py
@@ -1,12 +1,12 @@
 import pymongo
 
-class MongoHelper(object):
+class MongoDocHelper(object):
 
     def __init__(self,db_name,collection_name):
-        client = pymongo.MongoClient("mongodb://yang:yang#68@106.12.7.235:27017/admin")
-        db = client[db_name]
-        self.doc = db[collection_name]
-
+        self.client = pymongo.MongoClient("mongodb://yang:yang#68@106.12.7.235:27017/admin")
+        self.db = self.client[db_name]
+        self.doc =self.db[collection_name]
+ 
     def insert(self,data):
         doc_info=dict(data)
         self.doc.insert(doc_info)
--- test_py/test_note.py
+++ test_py/test_note.py
@@ -1,7 +1,10 @@
-from Helper.mongo_helper import MongoHelper
+from Helper.mongo_helper import MongoDocHelper
 import time
 #import tushare as ts
-mongo=MongoHelper('test','test')
+mongo=MongoDocHelper('test','test')
+print(mongo.db)
+print(mongo.db['admin'])
 mongo.insert({'name':'test','time':time.strftime('%Y-%m-%d %X', time.localtime())})
 print('ok')
 #time.strftime('%Y-%m-%d %X', time.localtime())
+
