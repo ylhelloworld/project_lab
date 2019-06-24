aa1ecdc66a535d68c905f2d18bc9f789dede7dc2
yanglong <598251812@qq.com>
2019-06-25 00:11:14 +0800

    rename the folder

--- data_anlyse/test_note.py
+++ data_anlyse/test_note.py
@@ -1,7 +1,8 @@
-from Helper.mongo_helper import MongoDocHelper
+#from Helper.mongo_helper import MongoDocHelper
+import Helper.mongo_helper
 import time
 #import tushare as ts
-mongo=MongoDocHelper('test','test')
+mongo=Helper.mongo_helper.MongoDocHelper('test','test')
 print(mongo.db)
 print(mongo.db['admin'])
 mongo.insert({'name':'test','time':time.strftime('%Y-%m-%d %X', time.localtime())})
